40c876caf30693feb5dccb4ac71a8b7ac3bae9f8
andysongsong <sxj930916@gmail.com>
2021-08-11 22:16:03 +0000

    add loadtweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,28 +1,3 @@
-const data = [
-  {
-    user: {
-      name: "Newton",
-      avatars: "https://i.imgur.com/73hZDYK.png",
-      handle: "@SirIsaac",
-    },
-    content: {
-      text: "If I have seen further it is by standing on the shoulders of giants",
-    },
-    created_at: 1461116232227,
-  },
-  {
-    user: {
-      name: "Descartes",
-      avatars: "https://i.imgur.com/nlhLi3I.png",
-      handle: "@rd",
-    },
-    content: {
-      text: "Je pense , donc je suis",
-    },
-    created_at: 1461113959088,
-  },
-];
-
 $(document).ready(function () {
   // Fake data taken from initial-tweets.json
 
@@ -62,7 +37,19 @@ $(document).ready(function () {
     return $tweet;
   };
 
-  renderTweets(data);
+  const loadtweets = function () {
+    $.ajax({
+      url: "/tweets",
+      method: "GET",
+      success: (tweet) => {
+        console.log(tweet);
+        createTweetElement(tweet);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  };
 
   const $form = $("#tweetForm");
   $form.on("submit", function (event) {
@@ -70,5 +57,6 @@ $(document).ready(function () {
     console.log("submit form");
     const tweetData = $(this).serialize();
     console.log("1111111", tweetData);
+    $.post("/tweets", tweetData).then(loadtweets);
   });
 });
