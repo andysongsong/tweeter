cceb555618267980345f3dcdb6291ccf74c3ddb4
andysongsong <sxj930916@gmail.com>
2021-08-11 21:51:04 +0000

    add event listener

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,29 +1,30 @@
+const data = [
+  {
+    user: {
+      name: "Newton",
+      avatars: "https://i.imgur.com/73hZDYK.png",
+      handle: "@SirIsaac",
+    },
+    content: {
+      text: "If I have seen further it is by standing on the shoulders of giants",
+    },
+    created_at: 1461116232227,
+  },
+  {
+    user: {
+      name: "Descartes",
+      avatars: "https://i.imgur.com/nlhLi3I.png",
+      handle: "@rd",
+    },
+    content: {
+      text: "Je pense , donc je suis",
+    },
+    created_at: 1461113959088,
+  },
+];
+
 $(document).ready(function () {
   // Fake data taken from initial-tweets.json
-  const data = [
-    {
-      user: {
-        name: "Newton",
-        avatars: "https://i.imgur.com/73hZDYK.png",
-        handle: "@SirIsaac",
-      },
-      content: {
-        text: "If I have seen further it is by standing on the shoulders of giants",
-      },
-      created_at: 1461116232227,
-    },
-    {
-      user: {
-        name: "Descartes",
-        avatars: "https://i.imgur.com/nlhLi3I.png",
-        handle: "@rd",
-      },
-      content: {
-        text: "Je pense , donc je suis",
-      },
-      created_at: 1461113959088,
-    },
-  ];
 
   const renderTweets = function (tweets) {
     for (let tweet of tweets) {
@@ -62,4 +63,12 @@ $(document).ready(function () {
   };
 
   renderTweets(data);
+
+  const $form = $("#tweetForm");
+  $form.on("submit", function (event) {
+    event.preventDefault();
+    console.log("submit form");
+    const tweetData = $(this).serialize();
+    console.log("1111111", tweetData);
+  });
 });
